9509a29e9ee5e88020361b1f642be2b67c01d4c5
Nigel Liang <nigel@nigelliang.com>
2023-05-25 14:17:26 +0800

    cleanup

--- qa.py
+++ qa.py
@@ -1,28 +1,22 @@
-"""Ask a question to the notion database."""
+import argparse
+
+from langchain import OpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
-from langchain import OpenAI, PromptTemplate
-from langchain.chains import VectorDBQAWithSourcesChain, RetrievalQAWithSourcesChain
-import argparse
 
 from prompts import QUESTION_PROMPT, COMBINE_PROMPT
 
 parser = argparse.ArgumentParser(description='Ask a question')
 parser.add_argument('question', type=str, help='The question to ask')
+parser.add_argument("--persist_db_location", type=str, help="Location on disk to persist the db", default="db")
 args = parser.parse_args()
 
 # Load the LangChain.
-PERSIST_DIRECTORY = "db"
-
 embedding = OpenAIEmbeddings()
-store = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embedding)
+store = Chroma(persist_directory=args.persist_db_location, embedding_function=embedding)
 retriever = store.as_retriever(search_type="mmr")
-# chain = RetrievalQAWithSourcesChain.from_llm(
-#     llm=OpenAI(temperature=0),
-#     question_prompt=QUESTION_PROMPT,
-#     combine_prompt=COMBINE_PROMPT,
-#     retriever=retriever
-# )
+
 chain = RetrievalQAWithSourcesChain.from_llm(
     llm=OpenAI(temperature=0),
     question_prompt=QUESTION_PROMPT,
